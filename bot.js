421154f5de1aba9c038b6ce5c2703ab4d3f68fef
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2021-01-20 16:05:44 +0100

    Working bot! May refactor to add more RSS news providers

--- bot.js
+++ bot.js
@@ -1,7 +1,13 @@
+'use strict'
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const schedule = require('node-schedule');
+
 const providers = require('./providers');
+
+/* -------------------------------------------------------------------------------- */
+/* --------------------- Bot configuration and global variables ------------------- */
+/* -------------------------------------------------------------------------------- */
 
 // Bot Token from environment
 const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -9,59 +15,111 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Where to save state data
-const NEWS_DIR = 'news_data';
+// List of news providers
+const HACKER_NEWS = new providers.HackerNewsProvider();
+const DAILY_SWIG = new providers.PortSwiggerProvider()
 
+const PROVIDERS = [
+    HACKER_NEWS,
+    DAILY_SWIG,
+]
 
-const PROVIDERS = [new providers.HackerNewsProvider()]
-
-// Schedule providers update
+// Schedule providers update at midnight
 PROVIDERS.forEach(provider => {
-    schedule.scheduleJob('0 0 * * *', provider.updateNews());
+    console.log(`[BOT] Scheduling update of ${provider.getProviderName()}`)
+    schedule.scheduleJob('0 0 0 * *', provider.updateNews());
 });
 
+// Schedule blacklist cleanup at the end of each month
+PROVIDERS.forEach(provider => {
+    console.log(`[BOT] Scheduling deletion of blacklist for ${provider.getProviderName()}`)
+    schedule.scheduleJob('0 0 0 1 *', provider.cleanBlacklist());
+});
 
+let USERS_WHITELIST = ['AlessandroZanattaK'];
+
+/* -------------------------------------------------------------------------------- */
+/* --------------------------------- Bot callbacks -------------------------------- */
+/* -------------------------------------------------------------------------------- */
 
 // Send help on "/help"
-bot.onText(/\/help/, (msg, _) => {
+bot.onText(/\/help/, (msg, match) => {
 
-    if(!isFromOwner){
+    if(!acceptedRequest(msg, match)){
         return;
     }
-  
+
     const chatId = msg.chat.id;
 
-    helpMessage = `
+    const helpMessage = `
     I can help you staying up to date with tech news!
     
-    Here are the commands to control me:
+Here are the commands to control me:
 
 
-    /help
-    /news
+    /help                        Show this help
+    /hackernews [new|top|best]   Get a random news from HackerNews
+    /swig                        Get list of news from PortSwigger's Daily Swig
     `;
+    console.log('hey3');
 
     bot.sendMessage(chatId, helpMessage);
 });
 
+// Send a new news on "/news"
+bot.onText(/\/hackernews(.*)/, (msg, match) => {
 
-bot.onText(/\/news(.)*/, (msg, match) => {
-
-    if(!isFromOwner){
+    if(!acceptedRequest(msg, match)){
         return;
     }
 
     const chatId = msg.chat.id;
 
-    bot.sendMessage(chatId, chatId);
+    const requestedType = match[1];
+
+    let chosenType = providers.NEWS_TYPES.new;
+    
+    if(requestedType.length > 0 && providers.NEWS_TYPES.isDefined(requestedType.substring(1))){
+        chosenType = providers.NEWS_TYPES.get(requestedType);
+    }
+
+    HACKER_NEWS.getNews(chosenType, url => {
+        bot.sendMessage(chatId, url);
+    });
 });
 
-// bot.onText(/\/best/, (msg, _) => {
-//     const chatId = msg.chat.id;
-// });
+bot.onText(/\/swig/, (msg, match) => {
+
+    if(!acceptedRequest(msg, match)){
+        return;
+    }
+
+    const chatId = msg.chat.id;
+
+    DAILY_SWIG.getNews(msg => {
+        bot.sendMessage(chatId, msg, {parse_mode : "MarkdownV2"});
+    })
+});
 
 
-function isFromOwner(id){
-    const ownerId = '1068272545';
-    return id === ownerId;
+/* -------------------------------------------------------------------------------- */
+/* ---------------------------------- Functions ----------------------------------- */
+/* -------------------------------------------------------------------------------- */
+
+
+function isWhitelisted(username){
+    return USERS_WHITELIST.includes(username);
+}
+
+function acceptedRequest(msg, calledOn){
+    const username = msg.chat.username;
+
+    console.log(`[BOT] ${username} --> ${calledOn[0]} `);
+
+    if(!isWhitelisted(username)){
+        console.log('    User is not in whitelist, dropping request...')
+        return false;
+    }
+    
+    return true;
 }
--- providers.js
+++ providers.js
@@ -1,9 +1,23 @@
-
 var request = require('then-request');
 const Enum = require('enum');
 const fs = require('fs');
+const random = require('random');
+let Parser = require('rss-parser');
+var escape = require('markdown-escape')
 
+// The directory containing the news
 const NEWS_DIR = 'news_data';
+
+// Create the directory if it does not exist
+if (!fs.existsSync(NEWS_DIR)){
+    fs.mkdirSync(NEWS_DIR);
+}
+
+// Type of news
+const NEWS_TYPES = new Enum(['new', 'top', 'best'], { ignoreCase: true })
+
+// RSS parser
+const PARSER = new Parser();
 
 /* -------------------------------------------------------------------------------- */
 /* ----------------------------------- Provider ----------------------------------- */
@@ -12,21 +26,72 @@ const NEWS_DIR = 'news_data';
 // Base class for the various news provider to extend
 
 class Provider {
+
+    constructor(providerName){
+        this.providerName = providerName;
+        this.providerDir = `${NEWS_DIR}/${this.providerName}`;
+        this.blacklistFile = `${this.providerDir}/blacklist.json`;
+
+        // Create directory of the provider, if it doesn't exists
+        if (!fs.existsSync(this.providerDir)){
+            fs.mkdirSync(this.providerDir);
+        }
+    }
+
+    getProviderName(){
+        return this.providerName;
+    }
+
     /**
      * Defines a method for updating the list of news.
      */
     updateNews(){};
 
-    /**
-     * Get the url of a news through an identifier.
+    /** 
+     * Pulls a new news of the requested type and passes it to the given callback
+     * 
+     * @param callback a function that requires a single argument, which is the message to send to the user
      */
-    getNewsUrl(identifier){};
+    getNews(callback){};
+
+
+    /**
+     * Cleans the blacklist, removing everything to avoid consuming too much space.
+     */
+    cleanBlacklist(){
+        console.log(`[${this.getProviderName()}] Cleaning blacklist...`);
+        fs.writeFile(this.blacklistFile, JSON.stringify([]), 'utf8', err => {
+            if(err) {
+                console.log(`    ${err}`);
+            }
+        });
+        console.log('    Done!')
+    }
+
+
+    /**
+     * Returns the blacklist to the callback
+     */
+    getBlacklist(callback){
+        fs.readFile(this.blacklistFile, (err, data) => {
+            callback(JSON.parse(data));
+        });
+    }
 
     /**
      * News that have already been sent to the user should not be sent again.
      * This function allows blacklisting of such news.
+     * 
+     * @param identifiers a list of object to blacklist
      */
-    blacklistNews(){};
+    addToBlacklist(currentBlacklist, identifiers){
+        currentBlacklist = currentBlacklist.concat(identifiers);
+        fs.writeFile(this.blacklistFile, JSON.stringify(currentBlacklist), err => {
+            if(err){
+                console.log(err);
+            }
+        });
+    }
 }
 
 /* -------------------------------------------------------------------------------- */
@@ -36,60 +101,178 @@ class Provider {
 class HackerNewsProvider extends Provider {
 
     constructor(){
-        super();
-        this.providerName = 'HackerNews';
+        super('HackerNews');
+
+        this.new = 'newstories.json';
+        this.top = 'topstories.json';
+        this.best = 'beststories.json';
+
+        this.news_types = new Enum({
+            'new': this.new, 
+            'best': this.best, 
+            'top': this.top
+        });
     }
 
     // Retrieve news ids from the HackerNews API
     updateNews(){
-        const HN_DIR = 'hackernews'
     
         // HackerNews valid API endpoints for getting news/story/ask ids
         const hackerNewsEndpoints = [
-            'topstories',
-            'beststories',
-            'newstories',
+            this.new,
+            this.top,
+            this.best
             // 'askstories',
             // 'showstories',
             // 'jobstories'
         ];
-    
-        console.log(`[-] Starting update of ${HN_DIR} ...`)
+        
+        console.log(`[${this.getProviderName()}] Starting update...`)
     
         hackerNewsEndpoints.forEach(endpoint => {
     
-            const url = `https://hacker-news.firebaseio.com/v0/${endpoint}.json`
+            const url = `https://hacker-news.firebaseio.com/v0/${endpoint}`
             
-            console.log(`   Got ${url}`);
+            console.log(`    Got ${url}`);
             
             request('GET', url).done(res => {
                 const data = res.getBody();
-                const filename = `${NEWS_DIR}/${HN_DIR}/${endpoint}.json`;
+                const filename = `${this.providerDir}/${endpoint}`;
                 fs.writeFile(filename, data, 'ascii', err => {
                     if(err){
-                        console.log(err);
-                    } else {
-                        console.log(`Saved "${endpoint}" ids to ${filename}`);
+                        console.log('    ' + err);
                     }
                 })
             });
         });
     }
-    
-    getNewsUrl(id, callback){
-    
-        const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
-    
-        console.log(`[+] Getting ${url}`)
-    
-        request('GET', url).done(res => {
-            callback(JSON.parse(res.getBody()).url);
+
+    getNews(type, callback){
+        if(!this.news_types.isDefined(type.key)){
+            callback(`This type of news is not available for ${this.getProviderName()} provider!`);
+            return;
+        }
+
+        console.log(`[${this.getProviderName()}] Searching for a '${type.key}' news...`)
+
+        // Chose a news from the correct file of ids
+        let idsFromFile = this.news_types.get(type.key).value;
+
+        const filePath = `${this.providerDir}/${idsFromFile}`;
+        fs.readFile(filePath, (err, data) => {
+            let ids = JSON.parse(data);
+
+            this.getBlacklist(blacklist => {
+                let notInBlacklist = ids.filter(x => !blacklist.includes(x));
+                let chosenID = notInBlacklist[random.int(0, notInBlacklist.length-1)];
+                this.getNewsUrl(chosenID, callback);
+                this.addToBlacklist(blacklist, chosenID);
+            });
         });
     }
 
-    blacklistNews(){
-        //TODO
+    getNewsUrl(id, callback){
+        
+        const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;    
+        request('GET', url).done(res => {
+            let url = JSON.parse(res.getBody()).url;
+
+            console.log(`[${this.getProviderName()}] Returning: ${url}`);
+            callback(url);
+        });
     }
+
+}
+
+/* -------------------------------------------------------------------------------- */
+/* ---------------------------------- PORTSWIGGER --------------------------------- */
+/* -------------------------------------------------------------------------------- */
+
+class PortSwiggerProvider extends Provider {
+
+    constructor(){
+        super('Daily Swig');
+
+        this.rssUrl = 'https://portswigger.net/daily-swig/rss';
+        this.rssFile = `${this.providerDir}/rss.xml`;
+    }
+
+    updateNews(){
+
+        console.log(`[${this.getProviderName()}] Starting update...`);
+        
+        request('GET', this.rssUrl).done(res => {
+            const data = res.getBody();
+            fs.writeFile(this.rssFile, data, 'utf8', err => {
+                if(err){
+                    console.log('    ' + err);
+                }
+            })
+        });
+
+        console.log('    Done!')
+    };
+
+    getNews(callback){
+
+        console.log(`[${this.getProviderName()}] Getting news...`)
+
+        fs.readFile(this.rssFile, 'utf8', (err, data) => {
+        if(err){
+            console.log('    ' + err);
+        } else{
+            // Async parse of RSS file
+            (async (data) => {
+                let feed = await PARSER.parseString(data);
+                let couples = [];
+                feed.items.forEach(item => {
+                    let title = item.title;
+                    let link = item.link;
+                    couples.push({'title': title, 'link': link});
+                });
+                
+                // With blacklist, check for already visited links
+                this.getBlacklist(blacklist => { 
+                    let notInBlacklist = couples.filter(x => !blacklist.includes(x.link));
+
+                    if(notInBlacklist.length == 0){
+                        callback('You\'ve read all the news! Check back tomorrow!');
+                        return;
+                    }
+                    
+                    const ITEMS_PER_MESSAGE = 5;
+                    
+                    let msg = `Here's ${ITEMS_PER_MESSAGE} news from Daily Swig:\n\n\n`;
+                    let toAddToBlack = [];
+                    
+                    for(let i = 0; i < Math.min(ITEMS_PER_MESSAGE, notInBlacklist.length); i++){
+                        let item = notInBlacklist[i];
+                        toAddToBlack.push(item.link);
+                        msg += `[${markdownEscape(item.title)}](${markdownEscape(item.link)})\n\n`;
+                    }
+                    
+                    this.addToBlacklist(blacklist, toAddToBlack);
+                    
+                    msg += '\n\n';
+                    callback(msg);
+                })
+            })(data);
+        }
+        });
+
+        console.log('    Done!')
+    };
+}
+
+
+/* -------------------------------------------------------------------------------- */
+/* ------------------------------------ HELPERS ----------------------------------- */
+/* -------------------------------------------------------------------------------- */
+
+function markdownEscape(string){
+    return string.replace(/\_|\*|\[|\]|\(|\)|\~|\`|\>|\#|\+|\-|\=|\||\{|\}|\.|\!/gi, x => {
+        return `\\${x}`;
+    });
 }
 
 /* -------------------------------------------------------------------------------- */
@@ -98,3 +281,5 @@ class HackerNewsProvider extends Provider {
 
 module.exports.HackerNewsProvider = HackerNewsProvider;
 module.exports.Provider = Provider;
+module.exports.PortSwiggerProvider = PortSwiggerProvider;
+module.exports.NEWS_TYPES = NEWS_TYPES;
